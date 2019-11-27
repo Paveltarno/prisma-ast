2c751816db2dc2f6302d2ec5767fb5659c3f2fe5
Pavel Tarno <paveltarno@gmail.com>
2019-11-27 15:55:12 +0100

    tests(printPrismaSchema)

---
+++ src/printer/tests/printPrismaSchema.test.ts
@@ -0,0 +1,107 @@
+import { printPrismaSchema } from '../printPrismaSchema'
+import {
+  IPrismaSchemaAST,
+  PrismaSchemaASTTypes,
+  IPrismaSchemaASTModelFieldPrimitive,
+  IPrismaSchemaASTModelFieldRelation,
+  IPrismaSchemaASTModel,
+} from '../../types/PrismaSchemaAst'
+
+describe('printPrismaSchema', () => {
+  it('prints a pretty string', () => {
+    const testAst: IPrismaSchemaAST = {
+      type: PrismaSchemaASTTypes.AST,
+      nodes: [
+        {
+          type: 'model',
+          name: 'User',
+          fields: [
+            {
+              name: 'id',
+              type: 'modeFieldPrimitive',
+              fieldType: 'Int',
+              isOptional: false,
+              attributes: [
+                {
+                  type: 'attribute',
+                  value: 'id',
+                },
+              ],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'name',
+              type: 'modeFieldPrimitive',
+              fieldType: 'String',
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'email',
+              type: 'modeFieldPrimitive',
+              fieldType: 'String',
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'age',
+              type: 'modeFieldPrimitive',
+              fieldType: 'Int',
+              isOptional: true,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'posts',
+              type: 'modelFieldRelation',
+              fieldType: 'Post',
+              hasMany: true,
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldRelation,
+          ],
+        },
+        {
+          type: 'model',
+          name: 'Post',
+          fields: [
+            {
+              name: 'id',
+              type: 'modeFieldPrimitive',
+              fieldType: 'Int',
+              isOptional: false,
+              attributes: [
+                {
+                  type: 'attribute',
+                  value: 'id',
+                },
+              ],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'title',
+              type: 'modeFieldPrimitive',
+              fieldType: 'String',
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'content',
+              type: 'modeFieldPrimitive',
+              fieldType: 'String',
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldPrimitive,
+            {
+              name: 'author',
+              type: 'modelFieldRelation',
+              fieldType: 'User',
+              hasMany: false,
+              isOptional: false,
+              attributes: [],
+            } as IPrismaSchemaASTModelFieldRelation,
+          ],
+        },
+      ] as IPrismaSchemaASTModel[],
+    }
+    const result = printPrismaSchema(testAst)
+    expect(result).toMatchSnapshot()
+  })
+})
